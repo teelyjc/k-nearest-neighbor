b43485927fdb3fe70a4d81a8ec2205fb86234e75
Tannatee Juchan <ju.tannatee2543@gmail.com>
2025-04-09 11:59:05 +0700

    feat: display iris

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import streamlit
 
+import pandas as pds
+import numpy as np
+
+from matplotlib import pyplot as plt
+
 streamlit.title("การจำแนกข้อมูลด้วยเทคนิค Machine Learning")
 streamlit.image("public/images/me.jpeg")
+
+col1, col2, col3 = streamlit.columns(3)
+
+with col1:
+  streamlit.header("Versicolor")
+  streamlit.image("public/images/iris1.jpg")
+
+with col2:
+  streamlit.header("Verginiga")
+  streamlit.image("public/images/iris2.jpg")
+
+with col3:
+  streamlit.header("Setosa")
+  streamlit.image("public/images/iris3.jpg")
